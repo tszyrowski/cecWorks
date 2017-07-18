502d63f9b54fc740f73a70da335c94a120c76933
Tomasz <tszyrowski@gmail.com>
2017-07-18 20:37:41 +0100

    added labels and button

--- src/cecFavorite.py
+++ src/cecFavorite.py
@@ -6,13 +6,26 @@ this is my aplication showing my favorite things
 '''
 import tkinter as tk
 
+def click(event):
+    print("cecylia feels good")
+    lblActivetise.configure(text = "hello people of the world")
+
+def leftClick(event):
+    print("awsome")
+    lblActivetise.configure(text = "hi hi hi")
+
 root = tk.Tk()
 
-lblActivetise = tk.Label(root, text = "my Activites",bg = "blue")
+lblActivetise = tk.Label(root, text = " I love my Activites",bg = "lightblue", font="Helvetica 16 bold italic")
 lblActivetise.pack()
 
-lblGames = tk.Label(root, text = "Animals", bg = "pink")
+lblGames = tk.Label(root, text = "Animals are great and I like them", bg = "pink")
 lblGames.pack()
+
+btnGames = tk.Button(root,text="push me")
+btnGames.bind("<Button-3>", click)
+btnGames.bind("<Button-1>", leftClick)
+btnGames.pack()
 
 root.mainloop()
 
